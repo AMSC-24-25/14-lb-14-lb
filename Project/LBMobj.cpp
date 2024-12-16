b565b1b99c4d1708ff689c81208770ea6a6d1eb1
tiker02 <angelo.jedy@gmail.com>
2024-12-16 16:54:53 +0000

    LBM class up to date

--- Project/LBMobj.cpp
+++ Project/LBMobj.cpp
@@ -1,4 +1,5 @@
 #include "LBMobj.hpp"
+#include <iostream>
 
 using Eigen::VectorXd;
 using Eigen::ArrayXd;
@@ -13,14 +14,16 @@ void LBM::init_equilibrium()
         {
             for(unsigned int z = 0; z < this->N.z; ++z)
             {
+                applyInitial(x,y,z);
+
                 double rho = get_rho(x,y,z);
                 VectorXd u = get_u(x,y,z);
                 double unorm = u.norm();
                 double omusq = 1.0 - 1.5*(unorm * unorm);
-                ArrayXd c3u = this->v.get_c() * (u * 3.0);
-                VectorXd f = this->v.get_w().array().cwiseproduct(1.0 * omusq + c3u.cwiseproduct(c3u*0.5 + 1.0));
+                ArrayXd c3u = this->v->get_c() * (u * 3.0);
+                VectorXd f = this->v->get_w().array() * (1.0 * omusq + c3u * (c3u*0.5 + 1.0));
 
-                savePopulation(x,y,z, &f);
+                savePopulation(x,y,z, f);
             }
         }
     }
@@ -50,7 +53,7 @@ void LBM::stream_collide_save()
 
                 double rhoinv = 1.0/rho;
 
-                VectorXd cf_rhoinv = (this->v.get_c().transpose() * stream) * rhoinv;
+                VectorXd cf_rhoinv = (this->v->get_c().transpose() * stream) * rhoinv;
                 VectorXd& u = cf_rhoinv;
                 set_u(x,y,z, u );
 
@@ -58,11 +61,11 @@ void LBM::stream_collide_save()
                 double unorm = u.norm();
                 double omusq = 1.0 - 1.5*(unorm * unorm);
                 u *= 3.0;
-                ArrayXd c3u = this->v.get_c() * (u * 3.0);
-                VectorXd twr = this->v.get_w() * tauinv * rho;
+                ArrayXd c3u = this->v->get_c() * (u * 3.0);
+                VectorXd twr = this->v->get_w() * tauinv * rho;
 
                 stream *= omtauinv;
-                stream += twr.array().cwiseproduct( omusq + c3u.cwiseproduct(1.0 + 0.5*c3u) ); 
+                stream += (twr.array() * ( omusq + c3u * (1.0 + 0.5*c3u) )).matrix(); 
 
                 this->savePopulation(x,y,z, stream);
             }
@@ -71,3 +74,20 @@ void LBM::stream_collide_save()
 }
 
 
+LBM::LBM(LBM::VelocitySet::StandardSet vSet, LBM::dimensions d,  double nu) : N(d), nu(nu)
+{
+    v = std::make_unique<VelocitySet>(vSet);
+    if(N.x == 0 || N.y == 0 || N.z == 0)
+    {
+        std::cerr << "Setting problem size to zero!!\n";
+        exit(1);
+    }
+    if((v->getD() == 1 && (N.y != 1 || N.z != 1)) || (v->getD() == 2 && N.z != 1))
+    {
+        std::cerr << "Incompatible size and velocity set dimension!! The problem is "  << v->getD() << "D\n";
+        exit(1);
+    }    
+    population = (double*) malloc(sizeof(double) * N.x * N.y * N.z * v->getQ());
+    rho = (double*) malloc(sizeof(double) * N.x * N.y * N.z);
+    u = (double*) malloc(sizeof(double) * N.x * N.y * N.z * v->getD());
+}
--- Project/LBMobj.hpp
+++ Project/LBMobj.hpp
@@ -10,7 +10,7 @@
  * Author: Orest Shardt
  *
  */
-#include <tuple>
+#include <memory>
 #include <Eigen/Dense>
 
 #ifndef __LBM_H
@@ -47,20 +47,19 @@ class LBM
 
 
 
-        VelocitySet v;
-        unsigned int scale;
-        struct N {
+        std::unique_ptr<VelocitySet> v;
+        struct dimensions {
             int x;
             int y;
             int z;
-        } N;
-        const double nu = 1.0/6.0;
+        } const N;
+        const double nu;
         const double tau = 3.0*nu+0.5;
         const double cs = 1.0/1.732;
 
         bool computeFlowProperties;
         bool quiet;
-        int step;
+        int step = 0;
 
         double *population;
         double *rho;
@@ -69,7 +68,69 @@ class LBM
     void init_equilibrium();
     void stream_collide_save();
 
+    LBM::LBM(LBM::VelocitySet::StandardSet vSet, LBM::dimensions d,  double nu);
+
+    inline const double get_rho(int x, int y, int z);
+    inline void set_rho(int x, int y, int z, double rho);
+    inline const Eigen::VectorXd get_u(int x, int y, int z);
+    inline void set_u(int x, int y, int z, const Eigen::VectorXd& u);
+    inline Eigen::VectorXd populationAdjacent(int x, int y, int z);
+    inline void savePopulation(int x, int y, int z, const Eigen::VectorXd& population);
+
+    inline int index_r(int x, int y, int z);
+    inline int index_u(int x, int y, int z);
+    inline int index_f(int x, int y, int z);
+
+
+
 };
 
 #endif /* __LBM_H */
 
+const double LBM::get_rho(int x, int y, int z) { return rho[index_r(x,y,z)]; }
+void LBM::set_rho(int x, int y, int z, double rho) { this->rho[index_r(x,y,z)] = rho; }
+
+const Eigen::VectorXd LBM::get_u(int x, int y, int z)
+{
+    Eigen::VectorXd u(v->getD());
+    for(int i = 0; i < v->getD(); ++i)
+    {
+        u(i) = this->u[index_u(x,y,z) + i];
+    } 
+
+    return u;
+}
+
+void LBM::set_u(int x, int y, int z, const Eigen::VectorXd& u)
+{
+    for(int i = 0; i < v->getD(); ++i)
+    {
+        this->u[index_u(x,y,z) + i] = u(i);
+    } 
+}
+
+Eigen::VectorXd LBM::populationAdjacent(int x, int y, int z)
+{
+    Eigen::VectorXd adj( v->getQ() );
+    const Eigen::MatrixXd& c = v->get_c();
+    for(int i = 0; i < v->getQ(); ++i)
+    {
+        int x_adj = x + c(i, 0);
+        int y_adj = y + (v->getD() > 1)? c(i, 1) : 0;
+        int z_adj = z + (v->getD() == 3)? c(i, 2) : 0;
+        adj(i) = this->population[index_f(x_adj, y_adj, z_adj) + i];
+    } 
+    return adj;
+}
+
+void LBM::savePopulation(int x, int y, int z, const Eigen::VectorXd& population)
+{
+    for(int i = 0; i < v->getQ(); ++i)
+    {
+        this->population[index_f(x,y,z) + i] = population(i);
+    }     
+}
+
+int LBM::index_r(int x, int y, int z) { return (N.y * z + y) * N.x + x; }
+int LBM::index_u(int x, int y, int z) { return index_r(x,y,z) * v->getD(); }
+int LBM::index_f(int x, int y, int z) { return index_r(x,y,z) * v->getQ(); }
