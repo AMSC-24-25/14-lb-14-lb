541767897b063f9d0462987a12a447c3315bda7f
Luca <lucacris72@gmail.com>
2025-01-26 13:37:27 +0000

    Fixed compile.sh

--- Project/compile.sh
+++ Project/compile.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-rm -f LBM.o seconds.o main.o sim
+rm -f ldc_sim
 
-CXXFLAGS="-std=c++17 -pedantic -O3 -Wall -fopenmp"
+CXXFLAGS="-std=c++17 -pedantic -O3 -Wall -fopenmp -I ${mkEigenInc} -fopenmp -O3"
 
-g++ ${CXXFLAGS} -c LBM.cpp -o LBM.o
-g++ ${CXXFLAGS} -c seconds.cpp -o seconds.o
-g++ ${CXXFLAGS} -c main.cpp -o main.o
- 
-g++ -fopenmp LBM.o seconds.o main.o -o sim -lrt
-
-rm -f LBM.o seconds.o main.o
 
 # for mac:
 # g++ LBM.o seconds.o main.o -o sim
 
 #classes at the moment
-#g++ -Wall -I ${mkEigenInc} -fopenmp LBMobj.hpp LBMobj.cpp VelocitySet.cpp LidDrivenCavity.hpp LidDrivenCavity.cpp  main.cpp -o ldc_sim
+g++ ${CXXFLAGS} LBMobj.hpp LBMobj.cpp VelocitySet.cpp LidDrivenCavity.hpp LidDrivenCavity.cpp  main.cpp -o ldc_sim
