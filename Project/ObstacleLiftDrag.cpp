aeae35077035be9754f1eefa9a05f53a0a842bd5
tiker02 <angelo.jedy@gmail.com>
2025-01-31 18:52:41 +0000

    Fixed naming and behaviour inside the obstacle

--- Project/ObstacleLiftDrag.cpp
+++ Project/ObstacleLiftDrag.cpp
@@ -4,21 +4,21 @@ using Eigen::VectorXd;
 
 namespace LatticeBoltzmannMethod{
 
-    ObstacleLiftDrag::ObstacleLiftDrag(unsigned int x0, unsigned int y0, unsigned int z0, unsigned int lenght, unsigned int height, unsigned int depth)
+    ObstacleLiftDrag::ObstacleLiftDrag(unsigned int x_start, unsigned int y_start, unsigned int z_start, unsigned int lenght, unsigned int height, unsigned int depth)
     {
-        this->x = x0;
-        this->y = y0;
-        this->z = z0;
+        this->x0 = x_start;
+        this->y0 = y_start;
+        this->z0 = z_start;
         this->length = lenght;
         this->height = height;
         this->depth = depth;
     }
 
-    bool ObstacleLiftDrag::is_inside_point(unsigned int x, unsigned int y, unsigned int z) const
+    inline bool ObstacleLiftDrag::is_inside_point(unsigned int x, unsigned int y, unsigned int z) const
     {
-        bool x_inside = (x >= this->x) && (x <= (this-> x + this->length));
-        bool y_inside = (y >= this->y) && (y <= (this-> y + this->height));
-        bool z_inside = (z >= this->z) && (z <= (this-> z + this->depth));
+        bool x_inside = (x >= this->x0) && (x <= (this-> x0 + this->length));
+        bool y_inside = (y >= this->y0) && (y <= (this-> y0 + this->height));
+        bool z_inside = (z >= this->z0) && (z <= (this-> z0 + this->depth));
         return x_inside && y_inside && z_inside;
     }
 
@@ -28,12 +28,14 @@ namespace LatticeBoltzmannMethod{
         bool inside = is_inside_point(x, y, z);
         if(!inside) return;
 
-        unsigned int obx = this->x;
-        unsigned int oby = this->y;
-        unsigned int obz = this->z;
-        unsigned int right = this->x + this->length;
-        unsigned int top = this->y + this->height;
-        unsigned int dep = this->z + this->depth;
+        
+
+        unsigned int obx = this->x0;
+        unsigned int oby = this->y0;
+        unsigned int obz = this->z0;
+        unsigned int right = this->x0 + this->length;
+        unsigned int top = this->y0 + this->height;
+        unsigned int dep = this->z0 + this->depth;
         double x_bounce = 0.0, y_bounce = 0.0, z_bounce = 0.0;
         if(x == right) x_bounce = -1.0;
         else if(x == obx) x_bounce = 1.0;
@@ -46,7 +48,7 @@ namespace LatticeBoltzmannMethod{
 
         if(x_bounce == 0.0 && y_bounce == 0.0 && z_bounce == 0.0)
         {
-            f = VectorXd::Zero(l.v->getQ());
+            f = l.get_rho(x, y, z) * l.v->get_w();
             return;
         }
 
@@ -61,6 +63,7 @@ namespace LatticeBoltzmannMethod{
                 //std::cout << "yessir" << std::endl;
             }
         }
+        //std::cout << "(" << x <<  "," << y << ", " << z <<  ")         ";
     }
 }
 /*
