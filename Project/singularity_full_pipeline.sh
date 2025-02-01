5a2023fd4135a5a71abc598feb5776772da04e96
fffeelipe <lfepiar@unal.edu.co>
2025-02-01 11:49:09 +0100

    cmake full

--- Project/singularity_full_pipeline.sh
+++ Project/singularity_full_pipeline.sh
@@ -2,13 +2,11 @@
 
 module load mpi/openmpi-x86_64
 
-CXXFLAGS="-std=c++17 -pedantic -O3 -Wall -fopenmp"
-
 # for mac:
 # g++ LBM.o seconds.o main.o -o sim
 
 #classes at the moment
-mpic++ ${CXXFLAGS} -I/usr/include LBM.hpp LBM.cpp VelocitySet.cpp LidDrivenCavity3D.hpp LidDrivenCavity3D.cpp ObstacleLiftDrag.hpp ObstacleLiftDrag.cpp  main.cpp -o ldc_sim_mpi 
+cmake . && make
 
 singularity  build --fakeroot --force lbm.sif conf.def
 
