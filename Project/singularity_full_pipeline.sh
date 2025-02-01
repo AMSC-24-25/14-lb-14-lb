d017208bb209f06b82a0e62a9f6850aaa97d69c5
fffeelipe <lfepiar@unal.edu.co>
2025-02-01 12:07:37 +0100

    fixed full pipeline

--- Project/singularity_full_pipeline.sh
+++ Project/singularity_full_pipeline.sh
@@ -8,8 +8,8 @@ CXXFLAGS="-std=c++17 -pedantic -O3 -Wall -fopenmp"
 # g++ LBM.o seconds.o main.o -o sim
 
 #classes at the moment
-mpic++ ${CXXFLAGS} -I/usr/include LBM.hpp LBM.cpp VelocitySet.cpp LidDrivenCavity3D.hpp LidDrivenCavity3D.cpp ObstacleLiftDrag.hpp ObstacleLiftDrag.cpp  main.cpp -o ldc_sim_mpi 
+cmake . && make
 
 singularity  build --fakeroot --force lbm.sif conf.def
 
-singularity exec --bind bin_results:$HOME/bin_results lbm.sif mpirun -np 8 ./ldc_sim_mpi
+singularity exec --bind bin_results:$HOME/bin_results lbm.sif mpirun -np 8 ./lbm_sim
